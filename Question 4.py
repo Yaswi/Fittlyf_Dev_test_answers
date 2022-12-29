49e46c0fc031c2767911e26a679136f7548b6d5d
19121A0534 <77147726+Yaswi@users.noreply.github.com>
2022-12-29 12:02:21 +0530

    Question 4

---
+++ Question 4.py
@@ -0,0 +1,7 @@
+#Question 4. Show by raising a error how tuple are immutable and also define what exactly immutability is in your own words.7
+
+# Program:
+tup=(1,2,3,4,5)
+#changing the value of the tuple
+tup[3]=7
+print(tup)
